f1d1593f7e5e4f0047fae62a3468241baab5a7db
Bryan Yee <yee.bryan.c@gmail.com>
2016-05-07 18:10:37 -0700

    'Word counting' information displays at the bottom of the page

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
-
+var wordCountString;
 
 function wordCounter(){
+	//Count all words
 	var wordList = document.getElementById('textString').value.split(" ");
 	var wordCount = wordList.length;
 
+	//Count each unique word
 	var wordCountList = {};
 	wordList.forEach( word => {
 		if (wordCountList[word] === undefined) { wordCountList[word] = 1 }
 		else { wordCountList[word]++ }
 	});
 
-	var wordCountString = "";			//WORD\t\tCOUNT\n";
+	//Create a summary of the counting information
+	wordCountString = "Total Word Count: " + wordCount + "\n\n";			//WORD\t\tCOUNT\n";
 	for(let word in wordCountList){
 		wordCountString += word + ":  " + wordCountList[word] + "\n";
 	}
 
-	//alert("Total Word Count: " + wordCount + "\n\n" + wordCountString);
-
 	showResults();
 }
 
-
 function showResults(){
-	document.getElementById('summary').setAttribute("display","block");
+	//Show the hidden 'Summary' div
+	var summary = document.getElementById('summary');
+	summary.style.display = "block";
+
+	//Add the summary information to the 'Summary' div
+	var summaryText = document.createTextNode(wordCountString);
+	summary.appendChild(summaryText);
 }
