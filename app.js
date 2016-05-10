70767cffe6faf3ec7abc160d49f54ac80d8d203e
Bryan Yee <yee.bryan.c@gmail.com>
2016-05-09 20:11:01 -0700

    Refrehes the summary results each time the Count! button is pressed, instead of adding a new summary at the bottom

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var wordCount, wordCountList;
+var wordCount, wordCountList, summary;
 
 function wordCounter(){
 	//Count all words, excluding punctuation (include hypens and apostrophes)
@@ -21,25 +21,32 @@ function wordCounter(){
 
 
 function createAndShowSummary(){
-	//Prepare DOM elements
-	var summary = document.getElementById('summary');
-	var createBreak = () => document.createElement('br');
+
+	//Prepare the 'Summary' div to display the results
+	if(summary !== undefined) summary.remove();
+
+	summary = document.createElement('div');
+	summary.id = 'summary';
+
+	var summaryHeader = document.createElement('h1');
+	summaryHeader.appendChild(document.createTextNode('Summary'));
+	summary.appendChild(summaryHeader);
 	
 	//Add the total word count to the 'Summary' div by appending a text node
-	var wordCountString = "Total Word Count: " + wordCount;
-	var summaryText = document.createTextNode(wordCountString);
+	var summaryText = document.createTextNode("Total Word Count: " + wordCount);
 	summary.appendChild(summaryText);
+
+	var createBreak = () => document.createElement('br');
 	summary.appendChild(createBreak());
 	summary.appendChild(createBreak());
 
 	//Add counts for each unique word to the 'Summary' div
 	for(let word in wordCountList){
-		wordCountString = word + ":  " + wordCountList[word];
-		summaryText = document.createTextNode(wordCountString);
+		summaryText = document.createTextNode(word + ":  " + wordCountList[word]);
 		summary.appendChild(summaryText);
 		summary.appendChild(createBreak());
 	}
 
-	//Show the hidden 'Summary' div by changing the diplay property
-	summary.style.display = "block";
+	//Add the 'Summary'div to the webpage
+	document.getElementById('container').appendChild(summary);
 }
